5d7a978b900f212d9b021fcf594741ae8138f450
Edward Nguyen <edward.nguyen@contemi.com.vn>
2020-11-10 11:59:44 +0700

    add dot in the end

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Edward Nguyen
+          Edward Nguyen.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-  Nothing change.
+          Nothing change.
         </a>
       </header>
     </div>
